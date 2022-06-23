2d0c8253a30616446df41fc26fe1e248ec3493e9
Felipe <65329845+fromerof@users.noreply.github.com>
2022-06-23 16:42:18 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/tabla')
+def tabla():
+    users = [
+    {'first_name' : 'Michael', 'last_name' : 'Choi'},
+    {'first_name' : 'John', 'last_name' : 'Supsupin'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+    return render_template("index.html",tablaUsuarios=users)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
